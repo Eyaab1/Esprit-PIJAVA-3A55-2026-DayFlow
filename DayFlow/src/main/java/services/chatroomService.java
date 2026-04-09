02ad6f0da472303152db5682d92eae9b956d8f85
mariem-ayari163 <ayari.mariem.1@esprit.tn>
2026-04-09 21:25:15 +0100

    update

--- DayFlow/src/main/java/services/chatroomService.java
+++ DayFlow/src/main/java/services/chatroomService.java
@@ -1,63 +1,66 @@
 package services;
 
 import model.Chatroom;
-import utils.MyConnection;
+import utils.DbConnexion;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatroomService implements CRUD<Chatroom, Integer> {
 
     private Connection cnx;
 
     public ChatroomService() {
-        cnx = MyConnection.getInstance().getCnx();
+        try {
+            cnx = DbConnexion.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException("Connexion BD échouée", e);
+        }
     }
 
     @Override
-    public void create(Chatroom chatroom) throws SQLException {
-        insert(chatroom);
-    }
+    public void create(Chatroom chatroom) throws SQLException { insert(chatroom); }
 
     @Override
     public void insert(Chatroom chatroom) throws SQLException {
         String sql = "INSERT INTO chatroom (goal_id, state, created_at) VALUES (?, ?, ?)";
-
         PreparedStatement ps = cnx.prepareStatement(sql);
         ps.setInt(1, chatroom.getGoalId());
         ps.setString(2, chatroom.getState());
         ps.setTimestamp(3, Timestamp.valueOf(chatroom.getCreatedAt()));
-
         ps.executeUpdate();
-
-        System.out.println("Chatroom created successfully 💬");
     }
 
     @Override
     public void update(Chatroom chatroom) throws SQLException {
         String sql = "UPDATE chatroom SET goal_id = ?, state = ? WHERE id = ?";
-
         PreparedStatement ps = cnx.prepareStatement(sql);
         ps.setInt(1, chatroom.getGoalId());
         ps.setString(2, chatroom.getState());
         ps.setInt(3, chatroom.getId());
-
         ps.executeUpdate();
-
-        System.out.println("Chatroom updated 🔄");
     }
 
     @Override
     public void delete(Integer id) throws SQLException {
         String sql = "DELETE FROM chatroom WHERE id = ?";
-
         PreparedStatement ps = cnx.prepareStatement(sql);
         ps.setInt(1, id);
-
         ps.executeUpdate();
+    }
 
-        System.out.println("Chatroom deleted ❌");
+    public List<Chatroom> getAll() throws SQLException {
+        List<Chatroom> list = new ArrayList<>();
+        ResultSet rs = cnx.createStatement().executeQuery("SELECT * FROM chatroom");
+        while (rs.next()) {
+            Chatroom c = new Chatroom();
+            c.setId(rs.getInt("id"));
+            c.setGoalId(rs.getInt("goal_id"));
+            c.setState(rs.getString("state"));
+            c.setCreatedAt(rs.getTimestamp("created_at").toLocalDateTime());
+            list.add(c);
+        }
+        return list;
     }
 }
--- DayFlow/src/main/java/services/messageService.java
+++ DayFlow/src/main/java/services/messageService.java
@@ -1,30 +1,30 @@
 package services;
 
 import model.Message;
-import utils.MyConnection;
+import utils.DbConnexion;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MessageService implements CRUD<Message, Integer> {
 
     private Connection cnx;
 
     public MessageService() {
-        cnx = MyConnection.getInstance().getCnx();
+        try {
+            cnx = DbConnexion.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException("Connexion BD échouée", e);
+        }
     }
 
     @Override
-    public void create(Message message) throws SQLException {
-        insert(message);
-    }
+    public void create(Message message) throws SQLException { insert(message); }
 
     @Override
     public void insert(Message message) throws SQLException {
         String sql = "INSERT INTO message (content, created_at, is_pinned, is_edited, chatroom_id, author_id) VALUES (?, ?, ?, ?, ?, ?)";
-
         PreparedStatement ps = cnx.prepareStatement(sql);
         ps.setString(1, message.getContent());
         ps.setTimestamp(2, Timestamp.valueOf(message.getCreatedAt()));
@@ -32,36 +32,42 @@ public class MessageService implements CRUD<Message, Integer> {
         ps.setBoolean(4, message.isEdited());
         ps.setInt(5, message.getChatroomId());
         ps.setInt(6, message.getAuthorId());
-
         ps.executeUpdate();
-
-        System.out.println("Message sent 💬");
     }
 
     @Override
     public void update(Message message) throws SQLException {
         String sql = "UPDATE message SET content = ?, is_pinned = ?, is_edited = ? WHERE id = ?";
-
         PreparedStatement ps = cnx.prepareStatement(sql);
         ps.setString(1, message.getContent());
         ps.setBoolean(2, message.isPinned());
         ps.setBoolean(3, message.isEdited());
         ps.setInt(4, message.getId());
-
         ps.executeUpdate();
-
-        System.out.println("Message updated ✏️");
     }
 
     @Override
     public void delete(Integer id) throws SQLException {
         String sql = "DELETE FROM message WHERE id = ?";
-
         PreparedStatement ps = cnx.prepareStatement(sql);
         ps.setInt(1, id);
-
         ps.executeUpdate();
+    }
 
-        System.out.println("Message deleted ❌");
+    public List<Message> getAll() throws SQLException {
+        List<Message> list = new ArrayList<>();
+        ResultSet rs = cnx.createStatement().executeQuery("SELECT * FROM message");
+        while (rs.next()) {
+            Message m = new Message();
+            m.setId(rs.getInt("id"));
+            m.setContent(rs.getString("content"));
+            m.setChatroomId(rs.getInt("chatroom_id"));
+            m.setAuthorId(rs.getInt("author_id"));
+            m.setPinned(rs.getBoolean("is_pinned"));
+            m.setEdited(rs.getBoolean("is_edited"));
+            m.setCreatedAt(rs.getTimestamp("created_at").toLocalDateTime());
+            list.add(m);
+        }
+        return list;
     }
 }
